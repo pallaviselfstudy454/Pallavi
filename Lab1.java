ab3c683bac76b10059ca28e78c6edef1b261def8
pallaviselfstudy454 <pallavinarole@gmail.com>
2025-01-25 14:17:50 +0530

    Add files via upload

---
+++ Lab1.java
@@ -0,0 +1,57 @@
+HELLO WORLD Program :
+
+package labs;
+
+public class hello 
+{
+	  public static void main(String[] args)
+	    {
+	        System.out.println("HELLO WORLD");
+	    }
+}
+
+Output: 
+	HELLO WORLD
+
+
+
+
+
+
+
+
+PRIMITIVE DATATYPE Program:
+
+package labs;
+
+public class demo
+{
+	public static void main(String args[]) 
+	{
+		char a = 'A';
+		int n = 12;
+		byte u = 2;
+		short d = 43;
+		double i = 4.234;
+		float p = 2.984f;
+		long s = 2345;
+		
+		System.out.println("char:"+a);
+		System.out.println("int :"+n);
+		System.out.println("byte :"+u);
+		System.out.println("short :"+d);
+		System.out.println("double :"+i);
+		System.out.println("float :"+p);
+		System.out.println("long :"+s);
+	}
+}
+
+
+Output :
+	char : A
+	int : 12
+	byte : 2
+	short : 43
+	double : 4.234
+	float : 2.984
+	long : 2345
